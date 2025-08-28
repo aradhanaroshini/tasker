da5fcc0f4372d574982ad18c824e40d4f1af63d3
aradhanaroshini <aradhanaroshini@gmail.com>
2025-08-28 21:25:18 +0530

    todays commits

--- work.js
+++ work.js
@@ -1,8 +1,17 @@
 const inputTask=document.getElementById('task');
 const taskList=document.getElementById('todo');
+const taskdeleted=document.getElementById('ul').addEventListener('click',removeTask());
 function addTask(){
     let li=document.createElement('li');
+    li.className="list";
     li.textContent=inputTask.value;
-    document.getElementById('todo').appendChild(li);
-    console.log(inputTask.value);
+    document.getElementById('ul').appendChild(li);
+    inputTask.value=' ';
+}
+function removeTask(){
+    let target=event.target;
+    if(true){
+        target.remove();
+    }
+    
 }
